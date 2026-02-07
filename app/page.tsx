cf129a6f91f21b3fe2f6aa430335878157c7abe4
Andre Camerino <camerinoandre@gmail.com>
2026-02-08 01:16:46 +1300

    clear home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+import React from 'react'
+
+const page = () => {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
