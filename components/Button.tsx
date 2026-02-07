74dffa8a9d73df7fd94f4a90c1b1c07cdd970228
Andre Camerino <camerinoandre@gmail.com>
2026-02-08 02:16:07 +1300

    add primary button variant

---
+++ components/Button.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+  variant?: "primary" | "secondary";
+  link?: string;
+  className?: string;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  variant = "primary",
+  link,
+  className = "",
+  disabled = false,
+}) => {
+  const variantClasses = {
+    primary: `
+      bg-(--color-primary) text-(--color-secondary) border-2 border-transparent
+    `,
+    secondary: `
+    `,
+  };
+
+  const baseClasses = `
+    inline-flex h-10 px-7 py-1 rounded-3xl
+    items-center justify-center select-none
+
+    cursor-pointer transition-colors duration-100
+
+    text-sm font-semibold
+    ${variantClasses[variant]}
+    ${disabled ? "opacity-50 cursor-not-allowed" : ""}
+    ${className}
+  `
+    .trim()
+    .replace(/\s+/g, " ");
+
+  const handleClick = (e: React.MouseEvent) => {
+    if (disabled) {
+      e.preventDefault();
+      return;
+    }
+    if (onClick) onClick();
+  };
+
+  if (link) {
+    return (
+      <Link href={link} className={baseClasses} onClick={handleClick}>
+        <h1>{children}</h1>
+      </Link>
+    );
+  }
+
+  return (
+    <div className={baseClasses} onClick={handleClick}>
+      <h1>{children}</h1>
+    </div>
+  );
+};
+
+export default Button;
