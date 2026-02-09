ef026be6f5b743ef59563b7ae8bcca7db4196643
Andre Camerino <camerinoandre@gmail.com>
2026-02-09 22:25:29 +1300

    create glass style

---
+++ styles/glass.ts
@@ -0,0 +1,8 @@
+export const glass: string =
+  `
+  bg-(--color-primary)/5
+  bg-[linear-gradient(180deg,rgba(255,255,255,0.2)_0%,rgba(255,255,255,0)_2%)]
+  backdrop-blur-[1px]
+  border border-neutral-100/10
+  shadow-md
+  `;
