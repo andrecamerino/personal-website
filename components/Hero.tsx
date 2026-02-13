582fe7b0f589304b6a09129662a3591cd16824c2
Andre Camerino <camerinoandre@gmail.com>
2026-02-13 15:43:15 +1300

    sm:hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,24 +26,25 @@ const Hero = () => {
   const typedText = useTypewriter(words, 100, 3000);
 
   return (
-    <div className="py-60 flex flex-col items-center gap-30 text-xl">
+    <div className="lg:my-60 my-40 flex flex-col items-center gap-30 text-xl">
       <div className="w-full max-w-6xl">
-        <p className="text-left pl-24 m-0 -mb-6">
+        <p className="text-right lg:text-left lg:pl-24 lg:pr-0 pr-16 m-0 -mb-6">
           <span className="wave">👋</span>, my name is
         </p>
         <h1
-          className="font-black text-9xl tracking-[-8] text-center m-0"
+          className="font-black text-center m-0"
           style={{ transform: "scaleY(0.8)" }}
         >
-          ANDRE CAMERINO
+          <span className="lg:text-9xl lg:tracking-[-8] text-8xl tracking-[-8]">ANDRE</span>{" "}
+          <span className="lg:text-9xl lg:tracking-[-8] text-7xl tracking-[-6]">CAMERINO</span>
         </h1>
-        <p className="text-right pr-16 m-0 -mt-6">
-          and I&apos;m <span className="font-bold">{typedText}</span> based in
-          New Zealand 🇳🇿
+        <p className="lg:text-right lg:pr-16 pl-10 m-0 lg:-mt-6 -mt-4">
+          and I&apos;m <span className="font-bold">{typedText}</span>
+          <br className="lg:hidden" /> based in New Zealand 🇳🇿
         </p>
       </div>
 
-      <div className="flex gap-16">
+      <div className="flex lg:flex-row flex-col lg:gap-16 gap-4">
         <Button
           className="group"
           onClick={() => setTheme("dark")}
