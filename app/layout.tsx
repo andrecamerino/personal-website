2c8e6416bc82868f8f1f917466dea536fa978964
Andre Camerino <camerinoandre@gmail.com>
2026-02-14 22:20:13 +1300

    fix default background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${montserrat.variable} antialiased scroll-smooth`}>
+      <body className={`${montserrat.variable} antialiased scroll-smooth bg-(--color-secondary)`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
