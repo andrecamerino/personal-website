490b4c7ce459e8bb9a45c65a52fa8b500eec6b5f
Andre Camerino <camerinoandre@gmail.com>
2026-02-16 12:43:09 +1300

    add new project tabs

--- components/Projects/ProjectSection.tsx
+++ components/Projects/ProjectSection.tsx
@@ -1,26 +1,57 @@
-'use client'
+"use client";
 
-import { projects } from "@/data/projects";
+import { useState } from "react";
+import { projects, CreativeCategory } from "@/data/projects";
 import SectionTitle from "../SectionTitle";
 import ProjectCard from "./ProjectCard";
 import { useTheme } from "@/context/ThemeContext";
+import { RevealWrapper } from "../RevealWrapper";
+import Button from "../Button";
 
 const ProjectSection = () => {
   const { currentTheme } = useTheme();
 
-  const currentProjects = currentTheme === "dark"
-    ? projects.developer : projects.creative
+  const [activeTab, setActiveTab] = useState<CreativeCategory>("videography");
+
+  const currentProjects =
+    currentTheme === "dark" ? projects.developer : projects.creative[activeTab];
 
   return (
-    <div id="next-section" className="flex flex-col justify-center items-center gap-10">
+    <div
+      id="next-section"
+      className="flex flex-col justify-center items-center gap-10"
+    >
       <SectionTitle>FEATURED PROJECTS</SectionTitle>
 
+      {currentTheme === "light" ? (
+        <div className="flex flex-col lg:flex-row gap-2">
+          <Button
+            onClick={() => setActiveTab("videography")}
+            variant={activeTab === "videography" ? "primary" : "secondary"}
+          >
+            Videography
+          </Button>
+          <Button
+            onClick={() => setActiveTab("photography")}
+            variant={activeTab === "photography" ? "primary" : "secondary"}
+          >
+            Photography
+          </Button>
+          <Button
+            onClick={() => setActiveTab("editing")}
+            variant={activeTab === "editing" ? "primary" : "secondary"}
+          >
+            Editing
+          </Button>
+        </div>
+      ) : (
+        ""
+      )}
+
       {currentProjects.map((p, index) => (
-        <ProjectCard
-          key={index}
-          project={p}
-          reverse={index % 2 === 1} // zig-zag layout
-        />
+        <RevealWrapper key={`${currentTheme}-${activeTab}-${p.title}`}>
+          <ProjectCard project={p} reverse={index % 2 === 1} />
+        </RevealWrapper>
       ))}
     </div>
   );
