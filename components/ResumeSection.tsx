86b43ff4b4e5bd2dd86957950c3fffc542781165
Andre Camerino <camerinoandre@gmail.com>
2026-02-21 20:05:01 +1300

    remove creative resume

--- components/ResumeSection.tsx
+++ components/ResumeSection.tsx
@@ -4,6 +4,9 @@ import { useTheme } from "@/context/ThemeContext";
 import SectionTitle from "./SectionTitle";
 import { RevealWrapper } from "./RevealWrapper";
 
+const developerResume = "/resume/andre-developer-resume.pdf";
+const creativeResume = null;
+
 const ResumeSection = () => {
   const { currentTheme } = useTheme();
   return (
@@ -11,26 +14,30 @@ const ResumeSection = () => {
       <SectionTitle>Resume / CV</SectionTitle>
       <RevealWrapper>
         <div className="flex flex-col lg:flex-row gap-4 lg:gap-16 items-center">
-          <a href="/resume/andre-developer-resume.pdf" target="_blank">
-            <Button
-              className="group"
-              variant={currentTheme === "light" ? "secondary" : "primary"}
-            >
-              Download Developer Resume &nbsp;
-              <span className="hidden group-hover:inline">🔋</span>
-              <span className="inline group-hover:hidden">🪫</span>
-            </Button>
-          </a>
-          <a href="/resume/andre-creative-resume.pdf" target="_blank">
-            <Button
-              className="group"
-              variant={currentTheme === "dark" ? "secondary" : "primary"}
-            >
-              Download Creative Resume &nbsp;
-              <span className="hidden group-hover:inline">📸</span>
-              <span className="inline group-hover:hidden">📷</span>
-            </Button>
-          </a>
+          {developerResume && (
+            <a href={developerResume} target="_blank">
+              <Button
+                className="group"
+                variant={currentTheme === "light" ? "secondary" : "primary"}
+              >
+                Download Developer Resume &nbsp;
+                <span className="hidden group-hover:inline">🔋</span>
+                <span className="inline group-hover:hidden">🪫</span>
+              </Button>
+            </a>
+          )}
+          {creativeResume && (
+            <a href={creativeResume} target="_blank">
+              <Button
+                className="group"
+                variant={currentTheme === "dark" ? "secondary" : "primary"}
+              >
+                Download Creative Resume &nbsp;
+                <span className="hidden group-hover:inline">📸</span>
+                <span className="inline group-hover:hidden">📷</span>
+              </Button>
+            </a>
+          )}
         </div>
       </RevealWrapper>
     </div>
