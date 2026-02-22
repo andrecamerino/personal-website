6b8ed000e121857b4ad17c12e98a423a5a7aca69
Andre Camerino <camerinoandre@gmail.com>
2026-02-22 14:41:41 +1300

    add video disclaimer

--- components/Fullscreen/FullscreenVideo.tsx
+++ components/Fullscreen/FullscreenVideo.tsx
@@ -2,21 +2,22 @@ import { Project } from "@/types/Project";
 
 const FullscreenVideo = ({ project }: { project: Project }) => {
   return (
-    <div className="flex flex-col gap-4 items-center">
-      <h1
+    <div className="flex flex-col gap-2 items-center">
+      <h3
         className="text-(--color-primary) text-2xl font-bold"
         autoCapitalize="characters"
       >
         {project.title}
-      </h1>
+      </h3>
       <video
         src={`projects/${project.video}`}
         loop
         autoPlay
         playsInline
         controls
-        className="w-full h-full object-cover rounded-3xl"
+        className="w-full h-full lg:w-[90%] lg:h-[90%] object-cover rounded-3xl"
       />
+      <p className="font-extralight text-xs">*reduced quality for smooth playback (i dont want to pay 😂)</p>
     </div>
   );
 };
