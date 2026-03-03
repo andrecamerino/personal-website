7b6e92c6a70eb54c6a94e741829c277ad4340cc9
Andre Camerino <camerinoandre@gmail.com>
2026-03-03 16:34:22 +1300

    add tags data

--- types/Project.ts
+++ types/Project.ts
@@ -1,7 +1,13 @@
 export interface Project {
   title: string;
   description: string;
+  tags?: Tag[];
   images?: string[];
   video?: string;
   link?: string;
+}
+
+export interface Tag {
+  name: string;
+  color?: string;
 }
