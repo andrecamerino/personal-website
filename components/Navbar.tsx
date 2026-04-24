88f5e8f2bb26cd95265713f7772b7d752ad21d36
Andre Camerino <camerinoandre@gmail.com>
2026-04-24 19:46:17 +1200

    add emoji button + make navbar sticky

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,33 @@
 "use client";
-
 import Button from "./Button";
 import { scrollToSection } from "@/utils/scrollToSection";
 import { useTheme } from "@/context/ThemeContext";
 
 const Navbar = () => {
-    const { currentTheme } = useTheme();
-    const emoji = currentTheme == "dark" ? "</>" : "📸"
+  const { currentTheme, setTheme } = useTheme();
+  
+  const emoji = currentTheme === "dark" ? "</>" : "📸";
+  
+  const handleEmojiButton = () => {
+    setTheme(currentTheme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <div className="h-16 flex justify-between items-center px-4">
-      <p className="text-2xl">{emoji}</p>
-      <Button
-        className="group"
-        variant="primary"
+    <div className="h-16 flex justify-between items-center px-4 fixed top-0 left-0 w-full z-50">
+      <button 
+        onClick={handleEmojiButton} 
+        className="l-1 text-2xl hover:cursor-pointer"
+      >
+        {emoji}
+      </button>
+      
+      <Button 
+        className="group variant-primary" 
         onClick={() => scrollToSection("contact")}
       >
-        Get In Touch&nbsp;<span className="hidden group-hover:block"> 📬</span>
-        <span className="block group-hover:hidden"> 📭</span>
+        Get In Touch&nbsp;
+        <span className="hidden group-hover:block">📬</span>
+        <span className="block group-hover:hidden">📭</span>
       </Button>
     </div>
   );
